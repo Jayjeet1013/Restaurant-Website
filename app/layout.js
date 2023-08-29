369e947023369fc33ba5588ed02d2537a46a8e48
Jayjeet1013 <jayjeet1013@gmail.com>
2023-08-29 23:24:24 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import Navbar from '../components/layout/Navbar'
+import Footer from '../components/layout/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar/>
+        {children}
+        <Footer/>
+        </body>
     </html>
   )
 }
