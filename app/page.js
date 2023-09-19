e26338508291f1f3157375277e5c0a941464c1cb
Jayjeet1013 <jayjeet1013@gmail.com>
2023-09-19 23:21:10 +0530

    done structure

--- app/page.js
+++ app/page.js
@@ -1,10 +1,28 @@
+import AboutUs from '@/components/home/AboutUs'
+import Blogs from '@/components/home/Blogs'
+import BookTable from '@/components/home/BookTable'
+import Chef from '@/components/home/Chef'
+import Customers from '@/components/home/Customers'
+import Gallery from '@/components/home/Gallery'
 import Hero from '@/components/home/Hero'
+import Laurels from '@/components/home/Laurels'
+import Menu from '@/components/home/Menu'
+import Subscribe from '@/components/home/Subscribe'
 import Image from 'next/image'
 
 export default function Home() {
   return (
       <div >
         <Hero/>
+        <AboutUs/>
+        <BookTable/>
+        <Menu/>
+        <Chef/>
+        <Customers/>
+        <Laurels/>
+        <Blogs/>
+        <Gallery/>
+        <Subscribe/>
       </div>
   )
 }
--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -1,8 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+  <div className='flex justify-between'>
+    <div>
+      GERICHT
+    </div>
+
+    <div className='flex'>
+Home
+    </div>
+
+
+    <div>
+      Login
+    </div>
+  </div>
+
   )
 }
 
